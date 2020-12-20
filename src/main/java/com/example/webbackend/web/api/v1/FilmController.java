792d684c5e73a78ea3f57bfb0e9b3ce563147a5c
Irina <yarygina.irina00@gmail.com>
2020-12-20 21:03:27 +0300

    fix

--- src/main/java/com/example/webbackend/web/api/v1/FilmController.java
+++ src/main/java/com/example/webbackend/web/api/v1/FilmController.java
@@ -1,13 +1,8 @@
 package com.example.webbackend.web.api.v1;
 
-import com.example.webbackend.business.entity.FilmEntity;
 import com.example.webbackend.business.service.FilmService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 import static com.example.webbackend.utils.ApiConstant.FILM;
 
@@ -21,13 +16,4 @@ public class FilmController {
         this.filmService = filmService;
     }
 
-    @GetMapping
-    public List<FilmEntity> getAll(){
-        return filmService.getAllFilm();
-    }
-
-    @GetMapping("/{id}") // url: localhost:8080/film/{id}
-    public FilmEntity getFilmById(@PathVariable Integer id){
-        return filmService.getFilm(id);
-    }
 }
